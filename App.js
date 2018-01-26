9ed48f8ef0d519368c3d6d25b5f7ce1b73928782
Toi-Zephyr <zhangjliang@mail2.sysu.edu.cn>
2018-01-27 02:41:47 +0800

    天气小应用 启动！

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hello</Text>
+        <Text>温习React native</Text>
+        <Text>卯足干劲刚一发</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
